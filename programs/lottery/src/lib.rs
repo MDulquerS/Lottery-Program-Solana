b5d4fc48170cdadb05f5be091df3056453782027
Muhib Dulquer <m.dulquer.sumon@gmail.com>
2024-12-04 22:46:58 +0600

    implemented InitializeLottery context

--- programs/lottery/src/lib.rs
+++ programs/lottery/src/lib.rs
@@ -1,4 +1,27 @@
 use anchor_lang::prelude::*;
+use anchor_lang::system_program;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token_interface::{mint_to, Mint, MintTo, TokenAccount, TokenInterface}
+};
+// use switchboard_on_demand::accounts::RandomnessAccountData;
+use anchor_spl::metadata::{
+    Metadata,
+    MetadataAccount,
+    CreateMetadataAccountsV3,
+    CreateMasterEditionV3,
+    SignMetadata,
+    SetAndVerifySizedCollectionItem,
+    create_master_edition_v3,
+    create_metadata_accounts_v3,
+    sign_metadata,
+    set_and_verify_sized_collection_item,
+    mpl_token_metadata::types::{
+            CollectionDetails,
+            Creator, 
+            DataV2,
+        },
+};
 
 declare_id!("9L5CvYnrL9VYqfKygm6sYbhzft2bBV52Gp78VLFU7XXu");
 
@@ -39,6 +62,52 @@ pub struct Initialize<'info> {
     pub token_lottery: Account<'info, TokenLottery>,
     pub system_program: Program<'info, System>,
 }
+
+#[derive(Accounts)]
+pub struct InitializeLottery<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    #[account(
+        init,
+        payer = payer,
+        mint::decimals = 0,
+        mint::authority = collection_mint,
+        mint::freeze_authority = collection_mint,
+        seeds = [b"collection_mint".as_ref()],bump
+    )]
+    pub collection_mint: InterfaceAccount<'info, Mint>,
+    #[account(
+        init,
+        payer = payer,
+        token::mint = collection_mint,
+        token::authority = collection_token_account,
+        seeds = [b"collection_associated_token".as_ref()],bump
+    )]
+    pub collection_token_account: InterfaceAccount<'info, TokenAccount>,
+    #[account(
+        mut,
+        seeds = [b"metadata",token_metadata_program.key().as_ref(), collection_mint.key().as_ref()],
+        bump,
+        seeds::program = token_metadata_program.key()
+    )]
+    ///CHECK: This account is checked by the metadata smart contract
+    pub metadata:UncheckedAccount<'info>,
+    #[account(
+        mut,
+        seeds = [b"metadata",token_metadata_program.key().as_ref(), collection_mint.key().as_ref(), b"edition"],
+        bump,
+        seeds::program = token_metadata_program.key()
+    )]
+    ///CHECK: This account is checked by the metadata smart contract
+    pub master_edition:UncheckedAccount<'info>,
+
+    pub token_metadata_program: Program<'info, Metadata>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub token_program: Interface<'info, TokenInterface>,
+
+    pub system_program: Program<'info, System>,
+}
+
 
 #[account]
 #[derive(InitSpace)]
